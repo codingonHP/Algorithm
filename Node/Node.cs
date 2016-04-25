ae0e6b5584a3d7c514b6e389b0e2a8529fe4055a
codingonHP <vishal_24_anand@hotmail.com>
2016-04-25 22:59:16 +0530

    Single linked list

--- Node/Node.cs
+++ Node/Node.cs
@@ -1,9 +1,24 @@
-﻿namespace Pluralsight.Algorithm
+﻿using System;
+
+namespace Pluralsight.Algorithm
 {
     class Node
     {
         static void Main(string[] args)
         {
+            var nodeItem = new NodeItem<int>();
+
+            nodeItem.AddItem(23);
+            nodeItem.AddItem(34);
+            nodeItem.AddItem(1);
+            nodeItem.AddItem(45);
+
+            foreach (var i in nodeItem.GetAll())
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Node/NodeItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Pluralsight.Algorithm
+{
+    public class NodeItem<T> 
+    {
+        public T Item { get; set; }
+        private NodeItem<T> NextPtr { get;  set; }
+        private NodeItem<T> HeadPtr { get; set; } 
+
+        public void AddItem(T item)
+        {
+            var newNode = new NodeItem<T>
+            {
+                Item = item,
+                NextPtr = null
+            };
+
+            if (HeadPtr == null)
+            {
+                HeadPtr = newNode;
+                return;
+            }
+
+            NodeItem<T> tempNodeItem = HeadPtr;
+            while (tempNodeItem.NextPtr != null)
+            {
+                tempNodeItem = tempNodeItem.NextPtr;
+            }
+
+            tempNodeItem.NextPtr = newNode;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            NodeItem<T> tempNodeItem = HeadPtr;
+            while ( tempNodeItem != null )
+            {
+                yield return tempNodeItem.Item;
+                tempNodeItem = tempNodeItem.NextPtr;
+            }
+        }
+    }
+}
