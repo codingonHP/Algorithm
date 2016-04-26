5bb0e0285f61c488eac1572a50a54b67890a2d00
codingonHP <vishal_24_anand@hotmail.com>
2016-04-26 21:33:45 +0530

    Added missing LinkedList.cs file

---
+++ Algorithm unit test project/NodeUnitTest.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm_unit_test_project
+{
+    [TestClass]
+    public class NodeUnitTest
+    {
+        [TestMethod]
+        public void CreateNewNodeTest()
+        {
+            var nodeItem = new Pluralsight.Algorithm.LinkedList<int>();
+
+            Assert.AreEqual(nodeItem.Count, 0);
+        }
+
+        [TestMethod]
+        public void AddItemToNodeListTest()
+        {
+            var nodeList = AddItemToNodeListAndReturn();
+            var allItemList = nodeList.GetAll();
+
+            Assert.AreEqual(nodeList.Count, allItemList.Count());
+        }
+
+        [TestMethod]
+        public void GetAllItemFromNodeList()
+        {
+            var nodeList = AddItemToNodeListAndReturn();
+            var allItemList = nodeList.GetAll();
+
+            var itemList = allItemList as int[] ?? allItemList.ToArray();
+
+            Assert.AreEqual(itemList.Count(), nodeList.Count);
+            Assert.AreEqual(itemList[0], 24);
+            Assert.AreEqual(itemList[1], 87);
+            Assert.AreEqual(itemList[2], 0);
+            Assert.AreEqual(itemList[3], -1);
+            Assert.AreEqual(itemList[4], -88);
+
+            try
+            {
+                Assert.AreEqual(itemList[5], 77);
+            }
+            catch (Exception exception)
+            {
+                Assert.IsInstanceOfType(exception, typeof(IndexOutOfRangeException) );
+            }
+
+        }
+
+        [TestMethod]
+        public void RemoveItemFromNodeListByPosition()
+        {
+            var nodeList = AddItemToNodeListAndReturn();
+
+            var allItems = nodeList.GetAll().ToArray();
+
+            var removeItem = nodeList.Remove(0);
+
+            Assert.AreEqual(removeItem, allItems[0]);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 1);
+
+            removeItem = nodeList.Remove(0);
+            Assert.AreEqual(removeItem, allItems[1]);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 2);
+
+            removeItem = nodeList.Remove(2);
+            Assert.AreEqual(removeItem, allItems[4]);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 3);
+
+
+        }
+
+        [TestMethod]
+        public void RemoveLastItemFromNodeList()
+        {
+            var nodeList = AddItemToNodeListAndReturn();
+
+            var allItems = nodeList.GetAll().ToArray();
+
+            var removeItem = nodeList.Remove(allItems.Length - 1 );
+
+            Assert.AreEqual(removeItem, allItems[allItems.Length - 1]);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 1);
+            
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void RemoveItemFromEmptyNodeList()
+        {
+            var emptyNodeList = new Pluralsight.Algorithm.LinkedList<int>();
+           emptyNodeList.Remove(0);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RemoveItemFromInvalidPositionInNodeList()
+        {
+            var nodeList = AddItemToNodeListAndReturn();
+            nodeList.Remove(-1);
+            nodeList.Remove(5);
+            nodeList.Remove(6);
+            nodeList.Remove(10);
+
+        }
+
+        [TestMethod]
+        public void RemoveItemFromNodeListByValue()
+        {
+            var nodeList = AddItemToNodeListAndReturn();
+            var allItems = nodeList.GetAll().ToArray();
+
+            var removedItemAt = nodeList.Remove(24,null);
+
+            Assert.AreEqual(removedItemAt, 0);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 1);
+
+            allItems = nodeList.GetAll().ToArray();
+            Assert.AreEqual(allItems[0], 87);
+
+            removedItemAt = nodeList.Remove(-1,Comparer<int>.Default);
+            Assert.AreEqual(removedItemAt, 2);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 1);
+
+            allItems = nodeList.GetAll().ToArray();
+
+            removedItemAt = nodeList.Remove(-88,null);
+            Assert.AreEqual(removedItemAt, 2);
+            Assert.AreEqual(nodeList.Count, allItems.Length - 1);
+
+            allItems = nodeList.GetAll().ToArray();
+            removedItemAt = nodeList.Remove(1000, null);
+
+            Assert.AreEqual(removedItemAt, -1);
+            Assert.AreEqual(nodeList.Count, allItems.Length);
+
+
+        }
+
+        [TestMethod]
+        public void GetEnumeratorTest()
+        {
+            var items = AddItemToNodeListAndReturn();
+            var enumerator = items.GetEnumerator();
+
+            Assert.IsNotNull(enumerator);
+            
+        }
+
+        [TestMethod]
+        public void LinkedListContainsItemTest()
+        {
+            var items = AddItemToNodeListAndReturn();
+            bool contains = items.Contains(23);
+
+            Assert.IsFalse(contains);
+        }
+
+
+        [TestMethod]
+        public void GroupBy()
+        {
+            Person[] arry =
+            {
+              new Person
+              {
+                  Age = 12,
+                  Address = "patna",
+                  Name = "vishal",
+                  Salary = 1200
+              },
+              new Person
+              {
+                  Age = 25,
+                  Address = "patna",
+                  Name = "vishal",
+                  Salary = 1000
+              },new Person
+              {
+                  Age = 23,
+                  Address = "patna",
+                  Name = "anand",
+                  Salary = 500
+              },new Person
+              {
+                  Age = 6,
+                  Address = "bangalore",
+                  Name = "piku",
+                  Salary = 700
+              },new Person
+              {
+                  Age = 6,
+                  Address = "bangalore",
+                  Name = "vikhyath",
+                  Salary = 1265
+              },new Person
+              {
+                  Age = 12,
+                  Address = "patna",
+                  Name = "delta",
+                  Salary = 3400
+              },new Person
+              {
+                  Age = 12,
+                  Address = "patna",
+                  Name = "sharad",
+                  Salary = 3500
+              },new Person
+              {
+                  Age = 23,
+                  Address = "patna",
+                  Name = "alisha",
+                  Salary = 900
+              },
+            };
+
+            var grpBy = arry.GroupBy(a => a.Age); //[12, 25, 23, 6]
+
+            var list = arry.GroupBy(a => a.Age, i => i.Name, (key, personNameList) =>
+            {
+                var enumerable = personNameList as string[] ?? personNameList.ToArray();
+                return enumerable;
+            }).ToList();
+
+
+            var listOfEvenLengthNames = arry.GroupBy(a => a.Age, 
+                                                    i => new {i.Name, i.Salary }, 
+                                                    (ageKey, personList) => new
+                                                    {
+                                                        Sal = personList.Max(p => p.Salary),
+                                                        Name = personList.FirstOrDefault(p => p.Salary == personList.Max(t => t.Salary)).Name
+                                                    })
+                                            .ToList();
+
+
+        }
+
+        public class Person
+        {
+            public int Age { get; set; }
+
+            public string Name { get; set; }
+
+            public string Address { get; set; }
+
+            public int Salary { get; set; }
+
+        }
+
+       
+
+
+        private Pluralsight.Algorithm.LinkedList<int> AddItemToNodeListAndReturn()
+        {
+            var nodeList = new Pluralsight.Algorithm.LinkedList<int>();
+            nodeList.Add(24);
+            nodeList.Add(87);
+            nodeList.Add(0);
+            nodeList.Add(-1);
+            nodeList.Add(-88);
+
+            return nodeList;
+        }
+    }
+}
---
+++ Node/LinkedList.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Pluralsight.Algorithm
+{
+
+
+    public class LinkedList<T> : ICollection<T>
+    {
+        public int Count { get; private set; }
+        public bool IsReadOnly { get; private set; }
+        private Node NodeList { get; set; }
+
+        public void Add(T item)
+        {
+            ++Count;
+            var newNode = new Node
+            {
+                Item = item,
+                Last = true,
+                Next = null
+            };
+
+            if (NodeList == null)
+            {
+                newNode.First = true;
+                NodeList = newNode;
+                return;
+            }
+
+
+            var tempNode = NodeList;
+            while (tempNode.Next != null)
+            {
+                tempNode.Last = false;
+                tempNode = tempNode.Next;
+            }
+
+            tempNode.Next = newNode;
+        }
+
+        public bool Remove(T item)
+        {
+            int removedAt = Remove(item, Comparer<T>.Default);
+            return removedAt != -1;
+        }
+
+        public T Remove(int position)
+        {
+            if (NodeList == null)
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+
+            if (position < 0 || position > Count - 1)
+            {
+                throw new ArgumentOutOfRangeException("position");
+            }
+
+            if (position == 0)
+            {
+                --Count;
+                var removedItem = NodeList;
+                NodeList = NodeList.Next;
+
+                if (NodeList != null)
+                {
+                    NodeList.First = true;
+                }
+
+                return removedItem.Item;
+            }
+
+            var tempNodeItem = NodeList;
+            Node prevNodeItem = null;
+
+            while (position != 0)
+            {
+                position--;
+                prevNodeItem = tempNodeItem;
+                tempNodeItem = tempNodeItem.Next;
+            }
+
+            if (prevNodeItem != null && tempNodeItem != null)
+            {
+                --Count;
+                prevNodeItem.Next = tempNodeItem.Next;
+                return tempNodeItem.Item;
+
+            }
+
+            --Count;
+            return prevNodeItem.Item;
+
+        }
+
+        public int Remove(T itemToRemove, IComparer<T> comparer)
+        {
+            if (NodeList == null)
+            {
+                throw new InvalidOperationException("Node list is empty");
+            }
+
+            var tempNode = NodeList;
+            Node tempPrevNode = null;
+
+            int postion = -1;
+
+            while (tempNode != null)
+            {
+                ++postion;
+
+                if (comparer != null)
+                {
+                    int compareResult = comparer.Compare(tempNode.Item, itemToRemove);
+                    if (compareResult == 0)
+                    {
+                        --Count;
+                        if (tempPrevNode != null)
+                        {
+                            tempPrevNode.Next = tempNode.Next;
+                        }
+                        else
+                        {
+                            NodeList = tempNode.Next;
+                            NodeList.First = true;
+                        }
+
+                        return postion;
+                    }
+                }
+                else if (tempNode.Item.Equals(itemToRemove))
+                {
+                    --Count;
+                    if (tempPrevNode != null)
+                    {
+                        tempPrevNode.Next = tempNode.Next;
+                    }
+                    else
+                    {
+                        NodeList = tempNode.Next;
+                        NodeList.First = true;
+                    }
+
+                    return postion;
+                }
+
+                tempPrevNode = tempNode;
+                tempNode = tempPrevNode.Next;
+
+            }
+
+            return -1;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            var tempNodeItem = NodeList;
+            while (tempNodeItem != null)
+            {
+                yield return tempNodeItem.Item;
+                tempNodeItem = tempNodeItem.Next;
+            }
+        }
+
+        public void Clear()
+        {
+            NodeList = null;
+        }
+
+        public bool Contains(T item)
+        {
+            if (typeof(T) == typeof(object) && item == null)
+            {
+                throw new ArgumentNullException("item is null");
+            }
+
+            var tempHead = NodeList;
+            while (tempHead != null)
+            {
+                if (tempHead.Item.Equals(item))
+                {
+                    return true;
+                }
+                tempHead = tempHead.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array is null");
+            }
+
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException("array index cannot be negative");
+            }
+
+            if (array.Length - arrayIndex < Count || arrayIndex >= Count)
+            {
+                throw new ArgumentOutOfRangeException("invalid array index");
+            }
+
+            var tempHead = NodeList;
+            while (tempHead != null)
+            {
+                array[arrayIndex++] = tempHead.Item;
+                tempHead = tempHead.Next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetAll() as IEnumerator<T>;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class Node
+        {
+            public T Item { get; set; }
+            public bool First { get; set; }
+            public bool Last { get; set; }
+            public Node Next { get; set; }
+
+        }
+    }
+
+}
