491a886dce4ddff4409d8e15b545ebd06a72acc0
codingonHP <vishal_24_anand@hotmail.com>
2016-04-26 21:29:36 +0530

    Updated Linked List and implemented ICollection<T>

---
+++ Node/ConsoleApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight.Algorithm
+{
+    class ConsoleApplication
+    {
+        static void Main(string[] args)
+        {
+            var nodeItem = new LinkedList<int>();
+
+            nodeItem.Add(23);
+            nodeItem.Add(34);
+            nodeItem.Add(1);
+            nodeItem.Add(45);
+
+           PrintNode(nodeItem);
+
+           int removedItem = nodeItem.Remove(0);
+
+            PrintNode(nodeItem);
+            Console.ReadKey();
+        }
+
+        private static void PrintNode(LinkedList<int> nodeList )
+        {
+            foreach (var i in nodeList.GetAll())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
